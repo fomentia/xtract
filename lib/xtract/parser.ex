3a047ebe29fe811cd65d3114f92c980deb0067ed
ijwilliams <isaac@thewilliams.ws>
2015-06-29 21:02:16 -0400

    Basic parsing

--- lib/xtract/parser.ex
+++ lib/xtract/parser.ex
@@ -1,9 +1,51 @@
 defmodule Xtract.Parser do
+  require Record
+  require Logger
+  Record.defrecord :xmlAttribute, Record.extract(:xmlAttribute, from_lib: "xmerl/include/xmerl.hrl")
+  Record.defrecord :xmlElement, Record.extract(:xmlElement, from_lib: "xmerl/include/xmerl.hrl")
+  Record.defrecord :xmlText, Record.extract(:xmlText, from_lib: "xmerl/include/xmerl.hrl")
+
   @doc """
     Xtract.Parser should take an XML document and xtract it (haha) into an Elixir datastructure.
   """
 
   def parse(xml) do
+    {doc, _} = xml |> :erlang.bitstring_to_list |> :xmerl_scan.string
+    elements = :xmerl_xpath.string('/book', doc)
+    
+    nodes = Enum.map(elements, fn(elem) ->
+      represent(xmlElement(elem, :content))
+    end)
 
+    nodes
+  end
+
+  defp represent(node) do
+    cond do
+      Record.is_record(node, :xmlElement) ->
+        name = xmlElement(node, :name)
+        content = xmlElement(node, :content)
+        Map.put(%{}, name, represent(content))
+
+      Record.is_record(node, :xmlText) ->
+        xmlText(node, :value) |> to_string
+
+      is_list(node) ->
+        case Enum.map(node, &(represent(&1))) do
+          [text_content] when is_binary(text_content) ->
+            text_content
+
+          elements ->
+            Enum.reduce(elements, %{}, fn(x, acc) ->
+              if is_map(x) do
+                Map.merge(acc, x)
+              else
+                acc
+              end
+            end)
+        end
+
+      true -> "cannot represent #{inspect node}"
+    end
   end
 end
